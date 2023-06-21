ca0a22f5d5e4255dc5303215e428d0bc652b4929
gmk0603 <57435225+gmk0603@users.noreply.github.com>
2023-06-21 18:13:54 +0900

    Add files via upload

---
+++ Web_Project/src/main/java/user/UserDTO.java
@@ -0,0 +1,86 @@
+package user;
+
+public class UserDTO {
+	private String userID;
+	private String userPassword;
+	private String userEmail;
+	private String userEmailHash;
+	private boolean userEmailChecked;
+	private String userName;
+	private String userBirth;
+	private String gender;
+	private String userPhone;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserEmailHash() {
+		return userEmailHash;
+	}
+	public void setUserEmailHash(String userEmailHash) {
+		this.userEmailHash = userEmailHash;
+	}
+	public boolean isUserEmailChecked() {
+		return userEmailChecked;
+	}
+	public void setUserEmailChecked(boolean userEmailChecked) {
+		this.userEmailChecked = userEmailChecked;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserBirth() {
+		return userBirth;
+	}
+	public void setUserBirth(String userBirth) {
+		this.userBirth = userBirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	
+	public UserDTO() {
+		
+	}
+	
+	public UserDTO(String userID, String userPassword, String userEmail, String userEmailHash, boolean userEmailChecked,
+			String userName, String userBirth, String gender, String userPhone) {
+		super();
+		this.userID = userID;
+		this.userPassword = userPassword;
+		this.userEmail = userEmail;
+		this.userEmailHash = userEmailHash;
+		this.userEmailChecked = userEmailChecked;
+		this.userName = userName;
+		this.userBirth = userBirth;
+		this.gender = gender;
+		this.userPhone = userPhone;
+	}
+}
